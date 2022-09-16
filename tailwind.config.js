e5440d3671598034aff84c5c873be76d906eb5bd
camila <112856775+Camilamellizo@users.noreply.github.com>
2022-09-16 14:30:27 +0200

    gaming

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './html/*.{html,js}',
+    './css/output.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
