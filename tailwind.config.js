8ea33cc6d448e331cab98b40f5ddc8e2d4a720d1
camila <112856775+Camilamellizo@users.noreply.github.com>
2022-09-20 13:26:11 +0200

    provecto mind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,25 @@ module.exports = {
     './css/output.css',
   ],
   theme: {
+    /* colors:{
+      'orangetransition':'#4A2800'
+
+    }, */
     extend: {
+      colors: {
+        orangef: '#4A2800',
+        blue: '#007ace',
+        red: '#de3618',
+        marron:'#241300'
+    
+      },
+      
       backgroundImage: {
         'control-play': "url('../images/logos.png')",
+        'montaña-rocosa': "url('../images/monte.png')",
+
       }
-    }/* ,
-    backgroundPosition: {
-      'center-right': 'right center',
-    } */
+    }
   },
   plugins: [],
   
